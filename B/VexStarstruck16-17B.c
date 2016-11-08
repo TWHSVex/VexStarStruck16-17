186f272f506a027b32b08530d6b0fed3b31b2f20
unknown <Student@003-319-S03.students.cisd.conroeisd.loc>
2016-11-08 16:11:04 -0600

    Added B code

--- B/VexStarstruck16-17B.c
+++ B/VexStarstruck16-17B.c
@@ -21,7 +21,7 @@ task main()
 	while(true)
 	{
 		tankControl(Ch2, Ch3);
-		if(vexRT(Btn5U)==1)
+		if(vexRT(Btn6D)==1)
 		{
 			motor[leftTop] = 127;
 			motor[leftMid]= 127;
@@ -48,12 +48,12 @@ task main()
 			motor[rightMid]=0;
 			motor[rightBot] = 0;
 		}
-		if(vexRT(Btn5D)==1)
+		if(vexRT(Btn5U)==1)
 		{
 			motor[leftClaw]=63;
 			motor[rightClaw]=63;
 		}
-		else if(vexRT(Btn6D)==1)
+		else if(vexRT(Btn5D)==1)
 		{
 			motor[leftClaw]=-63;
 			motor[rightClaw]=-63;
@@ -65,3 +65,48 @@ task main()
 		}
 	}
 }
+																																																																																	/*
+																																																																																	We're no strangers to love
+																																																																																	You know the rules and so do I
+																																																																																	A full commitment's what I'm thinking of
+																																																																																	You wouldn't get this from any other guy
+
+																																																																																	I just want to tell you how I'm feeling
+																																																																																	Gotta make you understand
+
+																																																																																	Never gonna give you up, never gonna let you down
+																																																																																	Never gonna run around and desert you
+																																																																																	Never gonna make you cry, never gonna say goodbye
+																																																																																	Never gonna tell a lie and hurt you
+
+																																																																																	We've known each other for so long
+																																																																																	Your heart's been aching but you're too shy to say it
+																																																																																	Inside we both know what's been going on
+																																																																																	We know the game and we're gonna play it
+
+																																																																																	And if you ask me how I'm feeling
+																																																																																	Don't tell me you're too blind to see
+
+																																																																																	Never gonna give you up, never gonna let you down
+																																																																																	Never gonna run around and desert you
+																																																																																	Never gonna make you cry, never gonna say goodbye
+																																																																																	Never gonna tell a lie and hurt you
+
+																																																																																	Never gonna give you up, never gonna let you down
+																																																																																	Never gonna run around and desert you
+																																																																																	Never gonna make you cry, never gonna say goodbye
+																																																																																	Never gonna tell a lie and hurt you
+
+																																																																																	We've known each other for so long
+																																																																																	Your heart's been aching but you're too shy to say it
+																																																																																	Inside we both know what's been going on
+																																																																																	We know the game and we're gonna play it
+
+																																																																																	I just want to tell you how I'm feeling
+																																																																																	Gotta make you understand
+
+																																																																																	Never gonna give you up, never gonna let you down
+																																																																																	Never gonna run around and desert you
+																																																																																	Never gonna make you cry, never gonna say goodbye
+																																																																																	Never gonna tell a lie and hurt you
+					 																																																																												*/
